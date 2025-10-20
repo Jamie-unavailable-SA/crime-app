4d8121d7fc7fe07dd411da30d0720733e81985d6
Jamie-unavailable-SA <jamie.kariuki@strathmore.edu>
2025-10-20 16:58:50 +0300

    initial mobile app screens creation pt 2

--- Mobile/crime_app/app/src/main/java/com/example/crime_app/ui/screens/LandingScreen.kt
+++ Mobile/crime_app/app/src/main/java/com/example/crime_app/ui/screens/LandingScreen.kt
@@ -1,2 +1,9 @@
 package com.example.crime_app.ui.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LandingScreen() {
+    Text("This is the landing screen")
+}
